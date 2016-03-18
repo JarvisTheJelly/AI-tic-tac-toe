94c277480f129d1d11f864558898396ffea5dd71
Nick Jarvis <najarvis2016@gmail.com>
2016-03-18 12:31:08 -0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,33 @@
+def print_board(b):                                                                                             
+        print b[0], b[1], b[2]                                                                                  
+        print b[3], b[4], b[5]                                                                                  
+        print b[6], b[7], b[8]                                                                                  
+                                                                                                                
+def check_if_won(player):                                                                                       
+        # possible_wins = [(0, 1, 2), (0, 3, 6), (0]
+        # NOT FINISHED HERE
+        
+def main():
+        board = [0, 0, 0,                                                                                       
+                 0, 0, 0,                                                                                       
+                 0, 0, 0]                                                                                       
+                                                                                                                
+        won = False                                                                                             
+        while not won:                                                                                          
+                print_board(board)                                                                              
+                # Player X play                                                                                 
+                valid_move = False                                                                              
+                while not valid move:                                                                           
+                        move = input("Enter move: ")                                                            
+                        try:
+                                if board[move] == 0:                                                            
+                                        board[move] = "X"                                                       
+                                        valid_move = True                                                       
+                        except Exception:                                                                       
+                                print "Not a valid move!"
+                # Check if won                                                                                  
+                                                                                                                
+                #Player O play                                                                                  
+                                                                                                                
+                # Check if won                                                                                  
+-- INSERT --                      
